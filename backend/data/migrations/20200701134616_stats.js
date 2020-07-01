4fa27ae00ffbee5b3dd045c38b957987d8b5c9d0
Adam Hoffman <wintros@yahoo.com>
2020-07-01 14:22:44 -0600

    table for stats setup

---
+++ backend/data/migrations/20200701134616_stats.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('stats', tbl => {
+      tbl.increments()
+      tbl.integer("PA")
+      tbl.integer("AB")
+      tbl.integer("BB")
+      tbl.integer("K")
+      tbl.integer("KL")
+      tbl.integer("Hits")
+      tbl.integer("2B")
+      tbl.integer("3B")
+      tbl.integer("HR")
+      tbl.integer("RBI")
+      tbl.integer("R")
+      tbl.integer("SB")
+      tbl.decimal("BA")
+      tbl.decimal("SLG")
+      tbl.decimal("OBP")
+      tbl.decimal("OPS")
+  })
+};
+
+exports.down = function(knex) {
+  
+};
