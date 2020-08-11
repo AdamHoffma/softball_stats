bc5326cfd3d84d1ced82319aa43b06db8efb4ea5
Adam Hoffman <wintros@yahoo.com>
2020-08-11 13:50:12 -0600

    playerlist done and styled, time to make the playerpage

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import axios from "axios"
 import Home from "./Components/home/home.js"
 import {Route} from "react-router-dom"
-import Players from "./Components/players/players.js"
 import playerForm from "./Components/playerForm/addPlayerForm.js"
 import editStats from "./Components/playerForm/editStats.js"
 import playerList from "./Components/playerpages/playerlist.js"
@@ -14,8 +13,7 @@ function App() {
 
   return (
     <div>
-      <Route exact path="/" component={Home} />
-      <Route path ="/players" component={Players} />
+      <Route exact path="/" component={Home} />      
       <Route path="/playerform" component={playerForm} />
       <Route path="/editstats" component={editStats} />
       <Route path='/playerlist' component={playerList} />
--- client/src/Components/playerpages/playerlist.js
+++ client/src/Components/playerpages/playerlist.js
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import {getPlayers} from "../../redux/actions.js"
 import { connect } from 'react-redux'
+import {Link} from "react-router-dom"
+import "./playerlist.css"
 
 const PlayerList = props => {
+
+    useEffect(() => {
+        props.getPlayers()
+    }, [])
+
+    const players = props.players
+
     return(
-        <div>Hi</div>
+        <div className="playerlistbody">
+            <div className="playerlistinnerbody">
+                <h1 className="playerlistheading">Click A Name To View Player Page</h1>
+                <div className="playerlist">
+                    {players.map(player => {
+                        return <div className="playerdiv">
+                                    <Link 
+                                        to={`/player/${player.id}`} 
+                                        className="link"
+                                    >
+                                        {player.Player}                                        
+                                    </Link>
+                               </div>
+                    })}
+                </div>
+            </div>
+        </div>
     )
 }
 
