55ef33cb93b04ced23a51b2d3ec59e12a1e626d5
Adam Hoffman <wintros@yahoo.com>
2020-08-24 13:22:57 -0600

    creating protected routes and login

---
+++ backend/auth/auth-middleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const secret = require('../secret/secret.js')
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization
+    if(token) {
+        jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+            if(err) {
+                res.status(401).json({message: "Token is not valid"})
+            } else {
+                req.user = {username: decodedToken.username}
+            }
+        })
+    } else {
+        res.status(401).json({message: "You shall not pass"})
+    }
+}
