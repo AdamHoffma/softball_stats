e178e284773289bba4b6a0dfc5ba8feb3cb26653
Adam Hoffman <wintros@yahoo.com>
2020-09-16 22:04:34 -0600

    forgot ID slug on route

--- backend/routes/schedule-router.js
+++ backend/routes/schedule-router.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
         })
 })
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
     const id = req.params.id
     Schedule.findById(id)
         .then(event => {
